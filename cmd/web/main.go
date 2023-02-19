0e5f62e4dd586037f69fcf6e986fbda59e375ab9
Stephanus Nugraha <mail@stephanusnugraha.com>
2023-02-19 11:57:05 +0700

    use pat for routing

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -27,12 +27,15 @@ func main() {
 
 	render.NewTemplates(&app)
 
-	http.HandleFunc("/", handlers.Repo.Home)
-	http.HandleFunc("/about", handlers.Repo.About)
-
 	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
-	err = http.ListenAndServe(portNumber, nil)
+
+	srv := &http.Server{
+		Addr:    portNumber,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
 	if err != nil {
-		log.Println(err)
+		log.Fatal(err)
 	}
 }
